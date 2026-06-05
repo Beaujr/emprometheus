7395268d5652df74ec68f558c63e78a791701853
Beau Rudder <beau.j.rudder@gmail.com>
2026-06-05 17:47:34 +0100

    some basic integration test to stop emhass version bumps costing me money

--- internal/prometheus/client.go
+++ internal/prometheus/client.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"time"
+
 	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
 	"github.com/prometheus/common/model"
-	"time"
 )
 
 type Reporter interface {
 	GetRange(ctx context.Context, query string, start, end time.Time, step time.Duration) (model.Matrix, error)
-	Query(ctx context.Context, query string, ts time.Time, opts ...v1.Option) (model.Value, v1.Warnings, error)
+	//Query(ctx context.Context, query string, ts time.Time, opts ...v1.Option) (model.Value, v1.Warnings, error)
 }
 
 var ErrNoRows = errors.New("no rows in result set")
@@ -51,4 +52,17 @@ func (r *reporter) GetRange(ctx context.Context, query string, start, end time.T
 		return m, nil
 	}
 	return nil, fmt.Errorf("unexpected type: %s", grid.Type().String())
+}
+
+// GetRange is a wrapper to handle ErrNoRows scenario and push the start back by 1 day
+func GetRange(ctx context.Context, r Reporter, query string, start time.Time, end time.Time, step int64) (model.Matrix, error) {
+	values, err := r.GetRange(ctx, query, start, end, time.Second*time.Duration(step))
+	if err != nil {
+		if errors.Is(err, ErrNoRows) {
+			// its empty due to no results, lets set the start to the previous day values as a backup
+			return r.GetRange(ctx, query, start.Add(-24*time.Hour), end, time.Second*time.Duration(step))
+		}
+		return nil, err
+	}
+	return values, nil
 }
